5b6f9385373820bd9bcd7f1933e8f7edab0bd2f5
BeatriceCruciani <beatric.cruciani@studenti.unicam.it>
2026-04-21 18:42:14 +0200

    scheletro progetto

---
+++ src/main/java/it/unicam/cs/mpgc/rpg123743/ui/javafx/JavaFXApp.java
@@ -0,0 +1,16 @@
+package it.unicam.cs.mpgc.rpg123743.ui.javafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
